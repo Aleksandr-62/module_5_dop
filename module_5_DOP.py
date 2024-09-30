050deeebe26207f7ab8663bb18a055dc5819bf31
Александр <shurik-zet@mail.ru>
2024-09-30 22:47:19 +0300

    Add files via upload

--- module_5_DOP.py
+++ module_5_DOP.py
@@ -27,8 +27,8 @@ class User:
 if __name__ == '__main__':
     database = Database()
     while True:
-        choiсe = int(input('Приветствую, выберите действие: \n1 - Вход\n2 - Регистрация\n'))
-        if choiсe == 1:
+        choice = int(input('Приветствую, выберите действие: \n1 - Вход\n2 - Регистрация\n'))
+        if choice == 1:
             login = input('Введите логин: ')
             password = input('Введите пароль: ')
             if login in database.data:
@@ -39,11 +39,11 @@ if __name__ == '__main__':
                     print('Неверный пароль')
             else:
                 print('Пользователь не найден')
-        if choiсe == 2:
+        if choice == 2:
             user = User(input('Введите логин: '), password := input('Введите пароль: '),
                         password2 := input('Повторите пароль: '))
             if password != password2:
                 print('Пароли не совпадают, попробуйте еще раз.')
                 continue
             database.add_user(user.username, user.password)
-      #  print(database.data)
+#  print(database.data)
